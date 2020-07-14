524b853f26ad1d13d39d209456c880a2d3320792
Dr. Shahin Rostami <hello@shahinrostami.com>
2020-07-14 21:29:25 +0100

    chord_rs now supports chord PRO

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::io::prelude::*;
 use nanoid::nanoid;
+use ureq::json;
 
 pub trait Plot {
     fn show(&self);
@@ -9,9 +10,11 @@ pub trait Plot {
 
 #[derive(Debug)]
 pub struct Chord {
+    pub user: String,
+    pub key: String,
     pub matrix: Vec<Vec<f64>>,
     pub names: Vec<String>,
-    pub colors: String,
+    pub colors: Vec<String>,
     pub opacity: f64,
     pub padding: f64,
     pub width: f64,
@@ -21,6 +24,21 @@ pub struct Chord {
     pub credit: bool,
     pub font_size: String,
     pub font_size_large: String,
+    pub details: Vec<Vec<Vec<String>>>,
+    pub details_thumbs: Vec<Vec<Vec<String>>>,
+    pub thumbs_width: f64,
+    pub thumbs_margin: f64,
+    pub thumbs_font_size: f64,
+    pub popup_width: f64,
+    pub noun: String,
+    pub details_separator: String,
+    pub divide: bool,
+    pub divide_idx: u64,
+    pub divide_size: f64,
+    pub instances: u64,
+    
+
+
 }
 
 impl Plot for Chord {
@@ -30,7 +48,14 @@ impl Plot for Chord {
         res = res.replace("${tag_id}", &format!("chart-{}", nanoid!()));
         res = res.replace("${matrix}", &format!("{:?}", self.matrix));
         res = res.replace("${names}", &format!("{:?}", self.names));
-        res = res.replace("${colors}", &self.colors);
+
+        if(self.colors.len() == 1){
+            res = res.replace("${colors}",&self.colors[0]);
+        }
+        else{
+            res = res.replace("${colors}", &format!("{:?}", self.colors));
+        }
+
         res = res.replace("${opacity}", &self.opacity.to_string());
         res = res.replace("${padding}", &self.padding.to_string());
         res = res.replace("${width}", &self.width.to_string());
@@ -45,31 +70,80 @@ impl Plot for Chord {
     }
 
     fn to_html(&self) {
-        let template_url = "https://shahinrostami.com/assets/chord/chord.tmpl";
-        let mut res = ureq::get(template_url).call().into_string().unwrap();
-        res = res.replace("${tag_id}", &format!("chart-{}", nanoid!()));
-        res = res.replace("${matrix}", &format!("{:?}", self.matrix));
-        res = res.replace("${names}", &format!("{:?}", self.names));
-        res = res.replace("${colors}", &self.colors);
-        res = res.replace("${opacity}", &self.opacity.to_string());
-        res = res.replace("${padding}", &self.padding.to_string());
-        res = res.replace("${width}", &self.width.to_string());
-        res = res.replace("${label_color}", &self.label_color);
-        res = res.replace("${wrap_labels}", &self.wrap_labels.to_string());
-        res = res.replace("${margin}", &self.margin.to_string());
-        res = res.replace("${credit}", &self.credit.to_string());
-        res = res.replace("${font_size}", &self.font_size);
-        res = res.replace("${font_size_large}", &self.font_size_large);
-        let file_name = "out.html";
+        if(self.user.is_empty() && self.key.is_empty()){
+            let template_url = "https://shahinrostami.com/assets/chord/chord_0_0_12.tmpl";
+            let mut res = ureq::get(template_url).call().into_string().unwrap();
+            res = res.replace("${tag_id}", &format!("chart-{}", nanoid!()));
+            res = res.replace("${matrix}", &format!("{:?}", self.matrix));
+            res = res.replace("${names}", &format!("{:?}", self.names));
 
-        let mut file = fs::File::create(file_name).unwrap();
-        file.write_all(res.as_bytes());
+            if(self.colors.len() == 1){
+                res = res.replace("${colors}",&self.colors[0]);
+            }
+            else{
+                res = res.replace("${colors}", &format!("{:?}", self.colors));
+            }
+            
+            res = res.replace("${opacity}", &self.opacity.to_string());
+            res = res.replace("${padding}", &self.padding.to_string());
+            res = res.replace("${width}", &self.width.to_string());
+            res = res.replace("${label_color}", &self.label_color);
+            res = res.replace("${wrap_labels}", &self.wrap_labels.to_string());
+            res = res.replace("${margin}", &self.margin.to_string());
+            res = res.replace("${credit}", &self.credit.to_string());
+            res = res.replace("${font_size}", &self.font_size);
+            res = res.replace("${font_size_large}", &self.font_size_large);
+            let file_name = "out.html";
+            
+            let mut file = fs::File::create(file_name).unwrap();
+            file.write_all(res.as_bytes());
+        }
+        else{
+            let template_url = "https://api.shahin.dev/chord";
+            
+            let mut res = ureq::post(template_url)
+            .auth(&self.user.to_string(),&self.key.to_string())
+            .send_json(json!({
+                "colors":self.colors,
+                "opacity":self.opacity,
+                "matrix":self.matrix,
+                "names":self.names,
+                "padding":self.padding,
+                "width":self.width,
+                "label_color":self.label_color,
+                "wrap_labels":self.wrap_labels,
+                "credit":self.credit,
+                "margin":self.margin,
+                "font_size":self.font_size,
+                "font_size_large":self.font_size_large,
+                "details":self.details,
+                "details_thumbs":self.details_thumbs,
+                "thumbs_font_size":self.thumbs_font_size,
+                "thumbs_width":self.thumbs_width,
+                "thumbs_margin":self.thumbs_margin,
+                "popup_width":self.popup_width,
+                "noun":self.noun,
+                "details_separator":self.details_separator,
+                "divide":self.divide,
+                "divide_idx":self.divide_idx,
+                "divide_size":self.divide_size,
+                "instances":self.instances
+            }))
+            .into_string().unwrap();
+
+            let file_name = "out.html";
+            
+            let mut file = fs::File::create(file_name).unwrap();
+            file.write_all(res.as_bytes());
+        }
     }
 }
 
 impl Default for Chord {
     fn default() -> Self {
         Chord {
+            user: String::new(),
+            key: String::new(),
             matrix: vec![
                 vec![0., 5., 6., 4., 7., 4.],
                 vec![5., 0., 5., 4., 6., 5.],
@@ -89,7 +163,7 @@ impl Default for Chord {
             .into_iter()
             .map(String::from)
             .collect(),
-            colors: "d3.schemeSet1".to_string(),
+            colors: vec![String::from("d3.schemeSet1")],
             opacity: 0.8,
             padding: 0.01,
             width: 700.0,
@@ -99,6 +173,18 @@ impl Default for Chord {
             credit: true,
             font_size: String::from("16px"),
             font_size_large: String::from("20px"),
+            details: vec![],
+            details_thumbs: vec![],
+            thumbs_width: 85.0,
+            thumbs_margin: 5.0,
+            thumbs_font_size: 14.0,
+            popup_width: 350.0,
+            noun: String::from("instances"),
+            details_separator: String::from(", "),
+            divide: false,
+            divide_idx: 0,
+            divide_size: 0.5,
+            instances: 0,
         }
     }
 }
---
+++ src/rich_hover.rs
@@ -0,0 +1,57 @@
+mod lib;
+use lib::{Chord, Plot};
+use std::fs;
+use std::io::prelude::*;
+use nanoid::nanoid;
+
+fn main() {
+    let matrix: Vec<Vec<f64>> = vec![
+    vec![0., 5., 6., 4., 7., 4.],
+    vec![5., 0., 5., 4., 6., 5.],
+    vec![6., 5., 0., 4., 5., 5.],
+    vec![4., 4., 4., 0., 5., 5.],
+    vec![7., 6., 5., 5., 0., 4.],
+    vec![4., 5., 5., 5., 4., 0.],
+    ];
+
+    let details : Vec<Vec<Vec<String>>> = vec![
+        vec![vec![], vec!["Movie 1".to_string(),"Movie 2".to_string()], vec!["Movie 3".to_string(),"Movie 4".to_string(),"Movie 5".to_string()], vec!["Movie 6".to_string(),"Movie 7".to_string()], vec!["Movie 8".to_string(),"Movie 9".to_string(),"Movie 10".to_string(),"Movie 11".to_string()], vec!["Movie 12".to_string()]],
+        vec![vec!["Movie 13".to_string(),"Movie 14".to_string()], vec![], vec!["Movie 15".to_string(),"Movie 16".to_string()], vec!["Movie 17".to_string()], vec!["Movie 18".to_string(),"Movie 19".to_string(),"Movie 20".to_string()], vec!["Movie 21".to_string(),"Movie 22".to_string()]],
+        vec![vec!["Movie 23".to_string(),"Movie 24".to_string(),"Movie 25".to_string()], vec!["Movie 26".to_string(),"Movie 27".to_string()], vec![], vec!["Movie 28".to_string()], vec!["Movie 29".to_string(),"Movie 30".to_string()], vec!["Movie 31".to_string(),"Movie 32".to_string()]],
+        vec![vec!["Movie 33".to_string()], vec!["Movie 34".to_string()], vec!["Movie 35".to_string()], vec![], vec!["Movie 36".to_string(),"Movie 37".to_string()], vec!["Movie 38".to_string(),"Movie 39".to_string()]],
+        vec![vec!["Movie 40".to_string(),"Movie 41".to_string(),"Movie 42".to_string(),"Movie 43".to_string()], vec!["Movie 44".to_string(),"Movie 45".to_string(),"Movie 46".to_string()], vec!["Movie 47".to_string(),"Movie 48".to_string()], vec!["Movie 49".to_string(),"Movie 50".to_string()], vec![], vec!["Movie 51".to_string()]],
+        vec![vec!["Movie 52".to_string()], vec!["Movie 53".to_string(),"Movie 54".to_string()], vec!["Movie 55".to_string(),"Movie 56".to_string()], vec!["Movie 57".to_string(),"Movie 58".to_string()], vec!["Movie 59".to_string()], vec![]]
+    ];
+
+    let details_thumbs : Vec<Vec<Vec<String>>> = vec![
+        vec![vec![], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string()]],
+        vec![vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec![], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string()]],
+        vec![vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec![], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string()]],
+        vec![vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec![], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string()]],
+        vec![vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec![], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string()]],
+        vec![vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string(),"https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec!["https://shahinrostami.com/images/stami-labs/lablet.png".to_string()], vec![]],
+    ];
+
+    let names: Vec<String> = vec![
+        "Action",
+        "Adventure",
+        "Comedy",
+        "Drama",
+        "Fantasy",
+        "Thriller",
+    ]
+    .into_iter()
+    .map(String::from)
+    .collect();
+
+    lib::Chord {
+        user: String::from("enter username here"),
+        key: String::from("enter license key here"),
+        matrix: matrix.clone(),
+        names: names.clone(),
+        details: details,
+        details_thumbs: details_thumbs,
+        ..lib::Chord::default()
+    }
+    .to_html();
+}
