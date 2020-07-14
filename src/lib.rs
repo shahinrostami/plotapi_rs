d8ebcaf57dcf1bcb5509f2e7024603136dfe1c6e
Dr. Shahin Rostami <hello@shahinrostami.com>
2020-07-14 22:22:01 +0100

    chord PRO now enabled for Jupyter Lab

--- src/lib.rs
+++ src/lib.rs
@@ -43,31 +43,69 @@ pub struct Chord {
 
 impl Plot for Chord {
     fn show(&self) {
-        let template_url = "https://shahinrostami.com/assets/chord/chord_0_0_12.tmpl";
-        let mut res = ureq::get(template_url).call().into_string().unwrap();
-        res = res.replace("${tag_id}", &format!("chart-{}", nanoid!()));
-        res = res.replace("${matrix}", &format!("{:?}", self.matrix));
-        res = res.replace("${names}", &format!("{:?}", self.names));
+        if(self.user.is_empty() && self.key.is_empty()){
+            let template_url = "https://shahinrostami.com/assets/chord/chord_0_0_12.tmpl";
+            let mut res = ureq::get(template_url).call().into_string().unwrap();
+            res = res.replace("${tag_id}", &format!("chart-{}", nanoid!()));
+            res = res.replace("${matrix}", &format!("{:?}", self.matrix));
+            res = res.replace("${names}", &format!("{:?}", self.names));
 
-        if(self.colors.len() == 1){
-            res = res.replace("${colors}",&self.colors[0]);
+            if(self.colors.len() == 1){
+                res = res.replace("${colors}",&self.colors[0]);
+            }
+            else{
+                res = res.replace("${colors}", &format!("{:?}", self.colors));
+            }
+
+            res = res.replace("${opacity}", &self.opacity.to_string());
+            res = res.replace("${padding}", &self.padding.to_string());
+            res = res.replace("${width}", &self.width.to_string());
+            res = res.replace("${label_color}", &self.label_color);
+            res = res.replace("${wrap_labels}", &self.wrap_labels.to_string());
+            res = res.replace("${margin}", &self.margin.to_string());
+            res = res.replace("${credit}", &self.credit.to_string());
+            res = res.replace("${font_size}", &self.font_size);
+            res = res.replace("${font_size_large}", &self.font_size_large);
+
+            println!("EVCXR_BEGIN_CONTENT text/html\n{}\nEVCXR_END_CONTENT", res);
         }
         else{
-            res = res.replace("${colors}", &format!("{:?}", self.colors));
+            let template_url = "https://api.shahin.dev/chord";
+            
+            let mut res = ureq::post(template_url)
+            .auth(&self.user.to_string(),&self.key.to_string())
+            .send_json(json!({
+                "colors":self.colors,
+                "opacity":self.opacity,
+                "matrix":self.matrix,
+                "names":self.names,
+                "padding":self.padding,
+                "width":self.width,
+                "label_color":self.label_color,
+                "wrap_labels":self.wrap_labels,
+                "credit":self.credit,
+                "margin":self.margin,
+                "font_size":self.font_size,
+                "font_size_large":self.font_size_large,
+                "details":self.details,
+                "details_thumbs":self.details_thumbs,
+                "thumbs_font_size":self.thumbs_font_size,
+                "thumbs_width":self.thumbs_width,
+                "thumbs_margin":self.thumbs_margin,
+                "popup_width":self.popup_width,
+                "noun":self.noun,
+                "details_separator":self.details_separator,
+                "divide":self.divide,
+                "divide_idx":self.divide_idx,
+                "divide_size":self.divide_size,
+                "instances":self.instances
+            }))
+            .into_string().unwrap();
+
+            println!("EVCXR_BEGIN_CONTENT text/html\n{}\nEVCXR_END_CONTENT", res);
+
         }
-
-        res = res.replace("${opacity}", &self.opacity.to_string());
-        res = res.replace("${padding}", &self.padding.to_string());
-        res = res.replace("${width}", &self.width.to_string());
-        res = res.replace("${label_color}", &self.label_color);
-        res = res.replace("${wrap_labels}", &self.wrap_labels.to_string());
-        res = res.replace("${margin}", &self.margin.to_string());
-        res = res.replace("${credit}", &self.credit.to_string());
-        res = res.replace("${font_size}", &self.font_size);
-        res = res.replace("${font_size_large}", &self.font_size_large);
-
-        println!("EVCXR_BEGIN_CONTENT text/html\n{}\nEVCXR_END_CONTENT", res);
-    }
+    } 
 
     fn to_html(&self) {
         if(self.user.is_empty() && self.key.is_empty()){
